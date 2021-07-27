c3e60a8a3e0e0bade53f3959308a6d0c0d8d5e05
mkornyev <mkornyev@gmail.com>
2021-07-27 18:56:58 -0400

    Basic Alarm Build Done
    -bootstrap layout
    -working clock
    -buttons!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// ================== RENDER SVA ======================
+
+function SetButton() {
+  // onclick -> set a specified time  
+  return (
+    <div className="col">
+      <button className="btn btn-lg btn-dark set">Set</button>
+    </div>
+  )
+}
+
+function SnoozeButton() {
+  // onclick -> 5 more minutes 
+  return (
+    <div className="col">
+      <button className="btn btn-lg btn-dark snooze">Snooze</button>
+    </div>
+  )
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: new Date(), // Browser Time
+    }
+  }
+
+  componentDidMount(){
+    this.timer = setInterval(() => (this.setState({ time: new Date() })), 1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer)
+  }
+
+  render() {
+    return (
+      <div className="row justify-content-md-center">
+        <div className="col-md-auto clock">
+          {this.state.time.toLocaleTimeString()}
+        </div>
+      </div>
+    )
+  }
+}
+
+class AlarmClock extends React.Component {
+  render() {
+    return(
+      <div className="container-fluid">
+        <Clock/>
+
+        <div className="row">
+          <SetButton/>
+          <SnoozeButton/>
+        </div>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AlarmClock/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
